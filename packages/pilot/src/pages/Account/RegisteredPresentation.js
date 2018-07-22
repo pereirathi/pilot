c367d80100e512189a047fd46ed2683d9c71833e
Vagner S <vagnervst17@gmail.com>
2018-07-22 18:01:23 -0300

    company: add signup request

---
+++ packages/pilot/src/pages/Account/RegisteredPresentation.js
@@ -0,0 +1,42 @@
+import React, { PureComponent } from 'react'
+import PropTypes from 'prop-types'
+import { withRouter } from 'react-router-dom'
+import { translate } from 'react-i18next'
+import { compose } from 'ramda'
+import RegisteredPresentation from '../../containers/Account/RegisteredPresentation'
+
+const enhanced = compose(
+  translate(),
+  withRouter
+)
+
+class RegisteredPresentationPage extends PureComponent {
+  constructor (props) {
+    super(props)
+    this.handleSignup = this.handleSignup.bind(this)
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  handleSignup (e) {
+    e.preventDefault()
+    this.props.history.push('/account/signup')
+  }
+
+  render () {
+    return (
+      <RegisteredPresentation
+        onGotoSignup={this.handleSignup}
+        t={this.props.t}
+      />
+    )
+  }
+}
+
+RegisteredPresentationPage.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }).isRequired,
+  t: PropTypes.func.isRequired,
+}
+
+export default enhanced(RegisteredPresentationPage)
---
+++ packages/pilot/src/pages/Account/UnregisteredPresentation.js
@@ -0,0 +1,45 @@
+import React, { PureComponent } from 'react'
+import PropTypes from 'prop-types'
+
+import { withRouter } from 'react-router-dom'
+import { translate } from 'react-i18next'
+
+import { compose } from 'ramda'
+
+import UnregisteredPresentation from '../../containers/Account/UnregisteredPresentation'
+
+const enhanced = compose(
+  translate(),
+  withRouter
+)
+
+class UnregisteredPresentationPage extends PureComponent {
+  constructor () {
+    super()
+    this.handleLogin = this.handleLogin.bind(this)
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  handleLogin (e) {
+    e.preventDefault()
+    this.props.history.push('/account/login')
+  }
+
+  render () {
+    return (
+      <UnregisteredPresentation
+        onBackToLogin={this.handleLogin}
+        t={this.props.t}
+      />
+    )
+  }
+}
+
+UnregisteredPresentationPage.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }).isRequired,
+  t: PropTypes.func.isRequired,
+}
+
+export default enhanced(UnregisteredPresentationPage)
